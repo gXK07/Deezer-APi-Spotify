4ea3e37a7b2f387f11481efb6dd0e59e9921cdef
gXK07 <80717523+gXK07@users.noreply.github.com>
2022-02-21 23:13:38 +0100

    /

--- src/player/player.js
+++ src/player/player.js
@@ -4,11 +4,18 @@ import icon_play_next from "../assets/skip_next_black_24dp.svg"
 import icon_play_previous from "../assets/skip_previous_black_24dp.svg"
 import mp3_sample from ".././assets/file_example_MP3_700KB.mp3"
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Player = () => {
-    const [time, setTime] = useState(70);
-    const [date, setDate] = useState(new Date())
-    console.log(date);
+    const [time, setTime] = useState(0);
+    const [timeLeft, setTimeLeft] = useState(100);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         setTime(time + 1);
+    //         console.log("time :" + time);
+    //     }, 1000);
+    //     return () => clearInterval(interval);
+    //   }, []);
     return <div className="player">
         <div className="controler">
             <button>
@@ -23,6 +30,7 @@ const Player = () => {
             </button>
         </div>
         <div className='timeLineDiv'>
+            {/* {timeLeft} */}
         <div className='timeLine'>
                 <div className='timePassed' style={{width : `${time}%`}}></div>
                 <button className='cursor'></button>
